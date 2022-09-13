ef5d0edadb02e97985477a95b1f9f49d7c0e7704
Roma <roma@MacBook-Pro-Roman.local>
2022-09-13 13:49:32 +0300

    add routing

---
+++ src/routes/reset-pass-page/reset-pass-page.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResetPassPage: React.FC = function () {
+  return <h2>Reset password</h2>;
+};
+
+export default ResetPassPage;
---
+++ src/routes/sign-up-page/sign-up-page.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUpPage: React.FC = function () {
+  return <h2>Sign-up page</h2>;
+};
+
+export default SignUpPage;
