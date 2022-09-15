bdde7e70a6c37369654121bde031149d8ee94c57
Roma <roma@MacBook-Pro-Roman.local>
2022-09-15 08:47:07 +0300

    add formik and hook form into project

--- src/routes/reset-pass-page/reset-pass-page.component.tsx
+++ src/routes/reset-pass-page/reset-pass-page.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ResetPassPage: React.FC = function () {
+const ResetPassPage: React.FC = function ResetPassPage() {
   return <h2>Reset password</h2>;
 };
 
---
+++ src/utils/styles/style-vars.tsx
@@ -0,0 +1,13 @@
+const globalStyles = {
+  vars: {
+    backgroundColor: "#363740",
+    secondaryBackgroundColor: "#f7f8fC",
+    whiteColor: "#fff",
+    blackColor: "#000",
+    fontColor: "#252733",
+    greyColor: "#9fa2b4",
+    hoveredColor: "#3751ff",
+  },
+};
+
+export default globalStyles;
