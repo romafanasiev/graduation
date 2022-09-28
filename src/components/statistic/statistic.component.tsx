b40df53c76035e0bd89544ae70e59e12c631d9d2
Roma <roma@MacBook-Pro-Roman.local>
2022-09-28 16:22:26 +0300

    add google analytics

---
+++ src/components/statistic/statistic.component.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Statistic: React.FC = function Statistic() {
+  return (
+    <iframe
+      title="statistic"
+      width="100%"
+      height="600"
+      src="https://datastudio.google.com/embed/reporting/3621712d-4614-4eb7-81e5-8baa707cea33/page/1M"
+      frameBorder="0"
+      allowFullScreen
+    />
+  );
+};
+
+export default Statistic;
--- src/routes/overview-page/overview-page.component.tsx
+++ src/routes/overview-page/overview-page.component.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Wrapper from "../../components/wrapper/wrapper.component";
+import Statistic from "../../components/statistic/statistic.component";
 
-const OverviewPage = function Overview() {
+const OverviewPage: React.FC = function Overview() {
   return (
     <Wrapper>
-      <h1>Overview Page</h1>
+      <Statistic />
     </Wrapper>
   );
 };
