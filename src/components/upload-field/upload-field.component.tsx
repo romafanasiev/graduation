4f85856c14ed1c8828614e5f6f1b6623846ee2cf
Roma <roma@MacBook-Pro-Roman.local>
2022-09-30 12:01:28 +0300

    add avatar preview for upload field

--- src/components/upload-field/upload-field.component.tsx
+++ src/components/upload-field/upload-field.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "@mui/material";
+import { Avatar, Button } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
 
 type Props = {
@@ -12,32 +12,40 @@ type Props = {
 
 const UploadField: React.FC<Props> = function UploadField({ setFieldValue }) {
   const [fileName, setFileName] = useState("Choose photo");
+  const [photo, setPhoto] = useState<string>("");
 
   return (
-    <Button
-      color="button"
-      variant="contained"
-      component="label"
-      fullWidth
-      sx={{ margin: "24px 0 0" }}
-      endIcon={<PhotoCamera color="inherit" />}
-    >
-      {fileName}
-      <input
-        hidden
-        accept="image/*"
-        multiple
-        type="file"
-        id="file"
-        name="file"
-        onChange={(event) => {
-          if (event.target.files) {
-            setFileName(event.target.files[0].name);
-            setFieldValue("file", event.target.files[0]);
-          }
-        }}
+    <>
+      <Avatar
+        sx={{ margin: "24px auto 0", width: 80, height: 80 }}
+        src={photo}
       />
-    </Button>
+      <Button
+        color="button"
+        variant="contained"
+        component="label"
+        fullWidth
+        sx={{ margin: "24px 0 0" }}
+        endIcon={<PhotoCamera color="inherit" />}
+      >
+        {fileName}
+        <input
+          hidden
+          accept="image/*"
+          multiple
+          type="file"
+          id="file"
+          name="file"
+          onChange={(event) => {
+            if (event.target.files) {
+              setPhoto(URL.createObjectURL(event.target.files[0]));
+              setFileName(event.target.files[0].name);
+              setFieldValue("file", event.target.files[0]);
+            }
+          }}
+        />
+      </Button>
+    </>
   );
 };
 
